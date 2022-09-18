6305999109513d93d7a0d9ba85e2a39529b419ab
Omar Alonso López Valenzuela <a01284179@itesm.mx>
2022-09-18 18:13:01 -0500

    Initial Commit

---
+++ SaludOSPrueba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaludOSPrueba
+//
+//  Created by Invitado on 18/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
